88dadb51794cdc4da0a4faee789a5c8c529070fd
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-06-02 01:54:41 +0500

    fix

--- diy.sh
+++ diy.sh
@@ -10,6 +10,3 @@
 
 # Modify hostname
 sed -i 's/OpenWrt/YouHua-WR1200JS/g' package/base-files/files/bin/config_generate
-
-# Modify the version number
-sed -i "s/OpenWrt /kyoto44 build $(TZ=UTC+5 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
