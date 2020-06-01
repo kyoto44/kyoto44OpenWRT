b42b2bf3d8a316cc3623264b3a3410781bdadbe5
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-06-02 01:04:20 +0500

    move to master

--- diy.sh
+++ diy.sh
@@ -7,3 +7,9 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/YouHua-WR1200JS/g' package/base-files/files/bin/config_generate
+
+# Modify the version number
+sed -i "s/OpenWrt /kyoto44 build $(TZ=UTC+5 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
